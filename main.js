900a6159253bd8b20de8779b035a06831c0e6b5a
yusufNinj1 <142718760+yusufNinj1@users.noreply.github.com>
2023-09-07 15:47:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
+
